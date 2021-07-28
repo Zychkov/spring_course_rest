8dce29ac53101e616b9b3be6fb62b747e594fa7b
Zychkov <48855208+Zychkov@users.noreply.github.com>
2021-07-29 00:17:37 +0700

    add CRUD operations

---
+++ src/main/java/ru/zychkov/spring/rest/exception_handling/NoSuchEmployeeException.java
@@ -0,0 +1,8 @@
+package ru.zychkov.spring.rest.exception_handling;
+
+public class NoSuchEmployeeException extends RuntimeException {
+
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+}
